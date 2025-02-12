7b858a1843ee2279f490b57bc5972180a016f569
Anneleen Scholts <anneleenscholts@hotmail.com>
2025-02-12 20:08:38 +0100

    Fixed the swagger opts

--- src/config/swaggerOpts.ts
+++ src/config/swaggerOpts.ts
@@ -32,8 +32,7 @@ export const swaggerOpts = {
   },
   servers: [
     {
-      // url: "https://syntra-20242025-henk-api.onrender.com/api",
-      url: "http://localhost:3000/api",
+      url: "https://syntra-20242025-henk-api.onrender.com/api",
       description: "The API server",
     },
   ],
